29497d13e209bc3b485491c7a18de8fbe9e90824
Taiji-Namba <67740063+Taiji-Namba@users.noreply.github.com>
2020-10-11 10:17:00 +0900

    Update app/models/user.rb
    Co-authored-by: T Tsujii <52148373+T-Tsujii@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-  # Database authenticable
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
-  
-  # Recoverable
-  validates :reset_password_token, presence: true
-  validates :reset_password_sent_at, presence: true
 end
