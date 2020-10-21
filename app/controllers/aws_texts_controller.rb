5cf20d769b4867ce8a509935e86f5549beddf6a3
hiroseg <hirosegawa3@gmail.com>
2020-10-21 07:11:41 +0700

    h2 とpに対するスタイルをcontainer-lgの入れ子に適応するように変更

--- app/controllers/aws_texts_controller.rb
+++ app/controllers/aws_texts_controller.rb
@@ -1,7 +1,6 @@
 class AwsTextsController < ApplicationController
   def index
-    # サーバ処理負担を減らすために必要なデータのみ選択して取得
-    @aws_texts = AwsText.select(:id, :title)
+    @aws_texts = AwsText.order(:id)
   end
   
 end
